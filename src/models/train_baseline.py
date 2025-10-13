aad6f4fb147fef519cfe6eaedacadd8099cbd999
illia <razyk.2006@gmail.com>
2025-10-13 18:10:08 +0100

    Data cleaning Preprocessing

--- src/models/train_baseline.py
+++ src/models/train_baseline.py
@@ -1,38 +1,86 @@
-import html
-import re
+
 import pandas as pd
+import re
+
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 """
 preprocessing
 """
 df = pd.read_csv("/home/illia/PycharmProjects/CommentGuard_ML/src/data/raw/youtoxic_english_1000.csv")
 
-def remove(df ,columns):
+df['Text'] = df['Text'].str.lower().str.strip()
 
-    df = df.copy()
-    df = df.drop(columns=columns)
-    return df
-
-# df = remove(df, ['CommentId' , 'VideoId'])
-# display(df.head())
+# print(df.describe())
+# print(df.dtypes)
+# print(df.isnull().sum())
 
 
 
+duplicate_rows = df[df.duplicated(subset=['Text'], keep=False)]
+# print('Duplicate rows based on "Text" column:')
+# print(duplicate_rows)
 
-def clean_text(s: str) -> str:
-    s = str(s)
-    s = html.unescape(s)                                # 1) Fix HTML
-    s = s.lower()                                       # 2) Lowercase
-    s = re.sub(r"http\S+|www\.\S+", " <url> ", s)       # 3) URLs
-    s = re.sub(r"\S+@\S+\.\S+", " <email> ", s)         # 4) Emails
-    s = re.sub(r"@[A-Za-z0-9_]+", " <user> ", s)        # 5) Mentions
-    s = re.sub(r"#([A-Za-z0-9_]+)", r" \1 ", s)         # 6) Hashtags
-    s = re.sub(r"[^a-z\s<>\']+", " ", s)                # 7) Remove non-letters
-    s = re.sub(r"\s+", " ", s).strip()                  # 8) Normalize spaces
-    return s if s else "<empty>"                        # Avoid empty strings
-
-
+"""
+                     tCommentId      VideoId           Text  IsToxic  ...  IsSexist  IsHomophobic  IsReligiousHate  IsRadicalism
+592        UgiXm5jxvkdIxHgCoAEC  cT14IbTDW2c  RUN THEM OVER     True  ...     False         False            False         False
+642  Ugxen2QgJYhNiRrMegR4AaABAg  cT14IbTDW2c  run them over     True  ...     False         False            False         False
+657  UgxXtUmfp0rdwXB8qld4AaABAg  cT14IbTDW2c  run them over     True  ...     False         False            False         False
+677  UgyjhPsMlWKlFNmG-h94AaABAg  cT14IbTDW2c  run them over     True  ...     False         False            False         False
+699  UgzFZGnqcjZcW7wejI54AaABAg  cT14IbTDW2c  RUN THEM OVER     True  ...     False         False            False         False
+"""
 
 
-df['Text'] = df['Text'].apply(clean_text)
-df['Text'].head()
+df.drop_duplicates(subset=["Text"],  keep="first", inplace=True)
+
+df.reset_index(drop=True , inplace=True)
+
+# print(df.iloc[642])
+
+"""
+tCommentId                                Ugxg0WuW6HaACEmc1Up4AaABAg
+VideoId                                                  cT14IbTDW2c
+Text               california is so stupid ! just arrest all them...
+IsToxic                                                         True
+IsAbusive                                                      False
+IsThreat                                                       False
+IsProvocative                                                  False
+IsObscene                                                      False
+IsHatespeech                                                    True
+IsRacist                                                        True
+IsNationalist                                                   True
+IsSexist                                                       False
+IsHomophobic                                                   False
+IsReligiousHate                                                False
+IsRadicalism                                                   False
+Name: 642, dtype: object
+"""
+
+
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"what's", "what is ", text)
+    text = re.sub(r"\'s", " ", text)
+    text = re.sub(r"\'ve", " have ", text)
+    text = re.sub(r"can't", "cannot ", text)
+    text = re.sub(r"n't", " not ", text)
+    text = re.sub(r"i'm", "i am ", text)
+    text = re.sub(r"\'re", " are ", text)
+    text = re.sub(r"\'d", " would ", text)
+    text = re.sub(r"\'ll", " will ", text)
+    text = re.sub(r"\'scuse", " excuse ", text)
+    text = re.sub('\W', ' ', text)
+    text = re.sub('\s+', ' ', text)
+    text = text.strip(' ')
+    return text
+
+'Important part to previous function to make every elem in "Text" to be cleaned by function'
+df['Text'] = df['Text'].map(lambda com : clean_text(com))
+
+print(df)
+
+
+vect = TfidfVectorizer(max_features=5000, stop_words='english')
+X = vect.fit_transform(df['Text'])
+Y = df["Toxic"]
